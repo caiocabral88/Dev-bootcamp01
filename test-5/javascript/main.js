bc7cd7981a86acb0d03630580cc6587e949a2057
caiocabral88 <caiocabral.job@gmail.com>
2025-05-17 20:04:31 -0300

    SpeechSynthesis

--- test-5/javascript/main.js
+++ test-5/javascript/main.js
@@ -1,3 +1,19 @@
+// document.querySelector('#enviar').addEventListener('click', rodar);
+
+
+// function rodar() {
+//     const nome1 = document.querySelector('#pnome').value;
+//     const nome2 = document.querySelector('#snome').value;
+//     const nome3 = document.querySelector('#tnome').value;
+//     const nome4 = document.querySelector('#qnome').value;
+
+//     document.querySelector('#grito').innerText = `${nome1} ${nome2} ${nome3} ${nome4}`;
+// }
+
+
+
+
+const sintese = window.speechSynthesis
 document.querySelector('#enviar').addEventListener('click', rodar);
 
 
@@ -7,5 +23,13 @@ function rodar() {
     const nome3 = document.querySelector('#tnome').value;
     const nome4 = document.querySelector('#qnome').value;
 
-    document.querySelector('#grito').innerText = `${nome1} ${nome2} ${nome3} ${nome4}`;
+    const grito1 = `${nome1} ${nome2} ${nome3} ${nome4}`;
+
+    document.querySelector('#grito').innerText = grito1;
+
+    let grite = new SpeechSynthesisUtterance(grito1);
+    grite.lang = 'pt-BR';  
+
+    sintese.speak(grite); 
 }
+
