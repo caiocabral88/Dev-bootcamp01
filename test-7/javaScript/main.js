dd2c47cb8f464def7b13446e689c4eae6ddc039e
caiocabral88 <caiocabral.job@gmail.com>
2025-05-17 21:54:14 -0300

    Exercício básico

---
+++ test-7/javaScript/main.js
@@ -0,0 +1,6 @@
+document.querySelector("#florDois").addEventListener("click", rodar);
+
+function rodar(){
+    document.querySelector("#florUm").style.display = "none";
+    document.querySelector("#florTres").style.display = "none";
+}
