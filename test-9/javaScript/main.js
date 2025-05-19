dd5af7fc6a5c207fe6689a61abbfd45079e33e79
caiocabral88 <caiocabral.job@gmail.com>
2025-05-19 14:49:37 -0300

    Comentários e organização do código Js

--- test-9/javaScript/main.js
+++ test-9/javaScript/main.js
@@ -1,3 +1,5 @@
+// Somador
+
 document.querySelector("#btnSomar").addEventListener("click", suma);
 
 let numero1 = document.getElementById("num1");
@@ -8,7 +10,7 @@ function suma() {
     alert("A sma é:" + numero3);
 }
 
-
+// Multiplicador de 3 Numeros
 document.querySelector("#btnMultiplicar").addEventListener("click", multiplicar);
 
 let multiplicar1 = document.getElementById("mult1");
@@ -19,4 +21,18 @@ function multiplicar() {
     let multiplicar4 = Number(multiplicar1.value) * Number(multiplicar2.value) * Number(multiplicar3.value);
 
     document.querySelector("#resultMult").innerText = multiplicar4;
+}
+
+// Resto da Divisão
+document.querySelector("#btnDividir").addEventListener("click", divisao);
+
+let dividir1 = document.getElementById("div1");
+let dividir2 = document.getElementById("div2");
+
+
+function divisao() {
+    let dividir3 = Number(dividir1.value) % Number(dividir2.value);
+
+    document.querySelector("#resultDiv").innerText = dividir3;
+
 }
