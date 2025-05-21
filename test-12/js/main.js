55b47144de817e86a32a95e685e3a7bab0c5e6b2
caiocabral88 <caiocabral.job@gmail.com>
2025-05-21 11:23:55 -0300

    Continuando desafio

---
+++ test-12/js/main.js
@@ -0,0 +1,17 @@
+document.querySelector("#botaoUm").addEventListener("click", subtrair);
+
+function subtrair() {
+    let valorSubUm = document.querySelector("#subUm").value;
+    let valorSubDois = document.querySelector("#subDois").value;
+
+    let resultSub = Number(valorSubUm) - Number(valorSubDois);
+    alert("Resultado da subtração: " + resultSub);
+}
+
+
+document.querySelector("#botaoDois").addEventListener("click", multiplicar);
+
+function multiplicar() {
+    let valorMultUm = document.querySelector("#multUm").value;
+    let valorMultDois = document.querySelector("#multDois").value;
+    let valorMultTres = document.querySelector("#multTres").value;
