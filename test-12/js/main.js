01deaffef1433dc9c193a260bd0cdd3b38d2aeb3
caiocabral88 <caiocabral.job@gmail.com>
2025-05-25 18:28:47 -0300

    Finalizando Parte 2 do desafio

--- test-12/js/main.js
+++ test-12/js/main.js
@@ -35,3 +35,19 @@ function dividir() {
     let resultDiv = Number(valDivUm) / Number(valDivDois) / Number(valDivTres);
     alert("Resultado da divisão: " + resultDiv);
 }
+
+
+// MEDIUM - JUST ONE STEP
+
+document.querySelector("#botaoQuatro").addEventListener("click", medium);
+    
+function medium() {
+    let valMedUm = document.querySelector("#mediumUm").value;
+    let valMedDois = document.querySelector("#mediumDois").value;
+    let valMedTres = document.querySelector("#mediumTres").value; 
+
+    let mediumValUm = Number(valMedUm) + Number(valMedDois);
+
+    let resultMedium = Number(mediumValUm) % Number(valMedTres);
+    alert("Resultado da expressão: " + resultMedium + " Sendo a soma dos dois primeiros: " + mediumValUm);
+}   
