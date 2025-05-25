318f2941d886ccd7402ae5ce3333fd7a25bc6130
caiocabral88 <caiocabral.job@gmail.com>
2025-05-25 19:41:06 -0300

    adicionado for ao exercicio

--- test-13/js/main.js
+++ test-13/js/main.js
@@ -2,11 +2,19 @@ document.querySelector("button").addEventListener("click", comer);
 
 
 
-let contador = 0;
-function comer(){
+// let contador = 0;
+// function comer(){
+//     let valor = document.getElementById("numUm").value;
+//     while (contador < Number(valor)){
+//         contador++;
+//         document.querySelector("#snack").innerText += " , " + contador;
+//     } 
+// }  
+
+
+function comer() {
     let valor = document.getElementById("numUm").value;
-    while (contador < Number(valor)){
-        contador++;
-        document.querySelector("#snack").innerText += " , " + contador;
-    } 
+    for (let i = 0; i < valor; i++) {
+        document.querySelector("#snack").innerText += " , " + (i + 1);
+    }
 }
