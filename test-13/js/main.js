0a1b89f4ac4760a068645e8590b1219b17c9ced0
caiocabral88 <caiocabral.job@gmail.com>
2025-05-26 02:55:05 -0300

    teste de for

--- test-13/js/main.js
+++ test-13/js/main.js
@@ -12,9 +12,18 @@ document.querySelector("button").addEventListener("click", comer);
 // }  
 
 
+// function comer() {
+//     let valor = document.getElementById("numUm").value;
+//     for (let i = 0; i < valor; i++) {
+//         document.querySelector("#snack").innerText += " , " + (i + 1);
+//     }
+// }
+
+
 function comer() {
-    let valor = document.getElementById("numUm").value;
-    for (let i = 0; i < valor; i++) {
+    let valor = document.getElementById("#textUm");
+    for (let i = 0; i < 366; i++) {
+        console.log(valor + " " + i);
         document.querySelector("#snack").innerText += " , " + (i + 1);
     }
 }
