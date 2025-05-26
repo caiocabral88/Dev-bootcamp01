2a0ecae8961fb5e8e9c6bcf1c9eea8abc5112ed0
caiocabral88 <caiocabral.job@gmail.com>
2025-05-26 03:25:17 -0300

    Mais execicios

--- test-13/js/main.js
+++ test-13/js/main.js
@@ -20,10 +20,24 @@ document.querySelector("button").addEventListener("click", comer);
 // }
 
 
-function comer() {
-    let valor = document.getElementById("#textUm");
-    for (let i = 0; i < 366; i++) {
-        console.log(valor + " " + i);
-        document.querySelector("#snack").innerText += " , " + (i + 1);
+// function comer() {
+//     let valor = document.getElementById("#textUm");
+//     for (let i = 0; i < 366; i++) {
+//         console.log(valor + " " + i);
+//         document.querySelector("#snack").innerText += " , " + (i + 1);
+//     }
+// }
+
+function addThree(n1, n2, n3) {
+    let value = Number(n1) + Number(n2) + Number(n3);
+    if (value > 1000) {
+        console.log("O valor é LINCOLN");
+    }
+}
+
+function dividThreee(n1, n2, n3) {
+    let result = Number(n1) / Number(n2) / Number(n3);
+    if (result % 5 === 0) {
+        alert("AE CARAI");
     }
 }
